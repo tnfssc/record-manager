aea3c56c83edad2b11a630e478bd33225be3c5da
tnfssc <tnfssc@gmail.com>
2022-02-05 01:09:35 +0530

    Added wouter

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { Router } from "wouter";
+
 import App from "./App";
 
 export default function Root() {
   return (
-    <>
-      <App />
-    </>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
   );
 }
