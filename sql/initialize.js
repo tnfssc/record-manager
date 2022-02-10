849c1f1a5fcb77398de7df8cffabde593e08715d
tnfssc <tnfssc@gmail.com>
2022-02-10 18:16:57 +0530

    New table added

--- sql/initialize.js
+++ sql/initialize.js
@@ -16,6 +16,17 @@ const initialize = async () => {
       });
       console.log("Students table created!");
     }
+    if (!(await db.schema.hasTable("columns"))) {
+      console.log("Creating Columns table...");
+      await db.schema.createTable("columns", (table) => {
+        table.integer("student_id").unsigned().primary().notNullable();
+        table.foreign("student_id").references("students.id");
+        table.timestamp("date").defaultTo(db.fn.now());
+        table.string("file_url_json").defaultTo(JSON.stringify([]));
+        table.timestamp("created_at").defaultTo(db.fn.now());
+      });
+      console.log("Columns table created!");
+    }
   } catch (error) {
     console.error(error);
   }
