a2fbcb071bcd3af08df32c12406c393b829b48ee
tnfssc <tnfssc@gmail.com>
2022-02-14 12:14:46 +0530

    Table API done

--- controllers/db/main.ts
+++ controllers/db/main.ts
@@ -5,12 +5,44 @@ import { Student } from "./students";
 export type Main = Student & { columns: Omit<Columns, "created_at">[] };
 
 /**@todo Fix limit issue */
+// const getMain = async (limit = 10, offset = 0): Promise<Main[]> =>
+//   await db<Student>("students")
+//     .orderBy("name")
+//     .limit(limit)
+//     .leftOuterJoin<Columns>("columns", "students.id", "=", "columns.student_id")
+//     .offset(offset)
+//     .orderBy("date")
+//     .select("*")
+//     .then((values) => {
+//       const result: Main[] = [];
+//       values.forEach(({ email, name, batch, id, created_at, ...value }) => {
+//         if (result.find((e) => e.id === id)) {
+//           result.find((e) => e.id === id).columns.push({ ...value });
+//         } else
+//           result.push({
+//             email,
+//             name,
+//             batch,
+//             id,
+//             created_at,
+//             columns: [{ ...value }],
+//           });
+//       });
+//       return result;
+//     });
 const getMain = async (limit = 10, offset = 0): Promise<Main[]> =>
   await db<Student>("students")
-    .orderBy("name")
-    .limit(limit)
-    .leftOuterJoin<Columns>("columns", "students.id", "=", "columns.student_id")
-    .offset(offset)
+    .with("students_list", (students_list) =>
+      students_list
+        .from<Student>("students")
+        .select("*")
+        .limit(limit)
+        .offset(offset)
+        .orderBy("email"),
+    )
+    .from<Student>("students_list")
+    .select("*")
+    .leftOuterJoin<Columns>("columns", "students_list.id", "=", "columns.student_id")
     .orderBy("date")
     .select("*")
     .then((values) => {
